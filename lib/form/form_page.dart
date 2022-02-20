5171eaffd00eeb8ac1dfab46ff24e445173639e7
jhoisz <jvrda@ufpi.edu.br>
2022-02-19 22:41:23 -0300

    feat: add mask to cep and phone number

--- lib/form/form_page.dart
+++ lib/form/form_page.dart
@@ -1,5 +1,7 @@
+import 'package:extended_masked_text/extended_masked_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:form_alura/address.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class FormPage extends StatefulWidget {
@@ -12,161 +14,187 @@ class FormPage extends StatefulWidget {
 class _FormPageState extends State<FormPage> {
   final _formKey = GlobalKey<FormState>();
 
+  Address? address;
+
+  final _nameController = TextEditingController();
+  final _phoneController = MaskedTextController(mask: '(000) 00000-0000');
+  final _cepController = MaskedTextController(mask: '00000-000');
+  final _numberController = TextEditingController();
+  final _streetController = TextEditingController();
+  final _neighborhoodController = TextEditingController();
+  final _complementController = TextEditingController();
+  final _cityController = TextEditingController();
+  final _stateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 50),
+          padding: const EdgeInsets.only(
+            top: 60,
+            left: 35.0,
+            right: 35.0,
+            bottom: 15.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Cadastro\nde endereço',
+                style: GoogleFonts.poppins(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    buildTextField(
+                      'Nome',
+                      TextInputType.name,
+                      context,
+                      _nameController,
+                    ),
+                    buildTextField(
+                      'Telefone',
+                      TextInputType.number,
+                      context,
+                      _phoneController,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: buildTextField(
+                            'CEP',
+                            TextInputType.number,
+                            context,
+                            _cepController,
+                          ),
+                        ),
+                        const SizedBox(width: 15.0),
+                        Expanded(
+                          child: buildTextField(
+                            'Número',
+                            TextInputType.phone,
+                            context,
+                            _numberController,
+                          ),
+                        ),
+                      ],
+                    ),
+                    buildTextField(
+                      'Logradouro',
+                      TextInputType.streetAddress,
+                      context,
+                      _streetController,
+                      maxLengthText: 100,
+                    ),
+                    buildTextField(
+                      'Bairro',
+                      TextInputType.text,
+                      context,
+                      _neighborhoodController,
+                    ),
+                    buildTextField(
+                      'Complemento',
+                      TextInputType.text,
+                      context,
+                      _complementController,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: buildTextField(
+                            'Estado',
+                            TextInputType.text,
+                            context,
+                            _stateController,
+                          ),
+                        ),
+                        const SizedBox(width: 15.0),
+                        Expanded(
+                          child: buildTextField(
+                            'Cidade',
+                            TextInputType.text,
+                            context,
+                            _cityController,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 16,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    primary: const Color(0xFF0123FF),
+                  ),
+                  onPressed: () {
+                    address = Address(
+                      name: _nameController.text,
+                      phone: _phoneController.text,
+                      cep: _cepController.text,
+                      number: _numberController.text,
+                      street: _streetController.text,
+                      neighborhood: _neighborhoodController.text,
+                      complement: _complementController.text,
+                      state: _stateController.text,
+                      city: _cityController.text,
+                    );
+
+                    debugPrint(address?.getAddress().toString());
+                  },
                   child: Text(
-                    'Cadastro\n de endereço',
+                    'Salvar',
                     style: GoogleFonts.poppins(
-                      fontSize: 30,
+                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      buildTextField(
-                        'Nome',
-                        TextInputType.name,
-                        context,
-                        1.2,
-                        8,
-                        MaxLengthEnforcement.none,
-                      ),
-                      buildTextField(
-                        'Telefone',
-                        TextInputType.number,
-                        context,
-                        1.2,
-                        12,
-                        MaxLengthEnforcement.enforced,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildTextField(
-                            'CEP',
-                            TextInputType.number,
-                            context,
-                            2.5,
-                            8,
-                            MaxLengthEnforcement.enforced,
-                          ),
-                          buildTextField(
-                            'Número',
-                            TextInputType.phone,
-                            context,
-                            2.5,
-                            8,
-                            MaxLengthEnforcement.enforced,
-                          ),
-                        ],
-                      ),
-                      buildTextField(
-                        'Logradouro',
-                        TextInputType.streetAddress,
-                        context,
-                        1.2,
-                        100,
-                        MaxLengthEnforcement.enforced,
-                      ),
-                      buildTextField(
-                        'Bairro',
-                        TextInputType.text,
-                        context,
-                        1.2,
-                        8,
-                        MaxLengthEnforcement.none,
-                      ),
-                      buildTextField(
-                        'Complemento',
-                        TextInputType.text,
-                        context,
-                        1.2,
-                        8,
-                        MaxLengthEnforcement.none,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildTextField(
-                            'Estado',
-                            TextInputType.text,
-                            context,
-                            2.5,
-                            8,
-                            MaxLengthEnforcement.none,
-                          ),
-                          buildTextField(
-                            'Cidade',
-                            TextInputType.text,
-                            context,
-                            2.5,
-                            8,
-                            MaxLengthEnforcement.none,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 1.2,
-                  height: MediaQuery.of(context).size.height / 16,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0)),
-                        primary: const Color(0xFF0123FF)),
-                    onPressed: () {
-                      // validate();
-                    },
-                    child: Text(
-                      'Salvar',
-                      style: GoogleFonts.poppins(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget buildTextField(String name, TextInputType type, BuildContext context,
-      num width, int maxLengthText, MaxLengthEnforcement isLimited) {
+  Widget buildTextField(
+    String name,
+    TextInputType type,
+    BuildContext context,
+    TextEditingController controller, {
+    int? maxLengthText,
+  }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           name,
-          style: GoogleFonts.poppins(fontSize: 18),
+          style: GoogleFonts.poppins(fontSize: 16),
         ),
         SizedBox(
-          width: MediaQuery.of(context).size.width / width,
+          width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height / 16,
           child: TextFormField(
+            controller: controller,
             // validator: (value) =>
             //     value!.isEmpty ? 'Email cannot be blank' : null,
             keyboardType: type,
             maxLength: maxLengthText,
-            maxLengthEnforcement: isLimited,
+            maxLengthEnforcement:
+                maxLengthText != null ? MaxLengthEnforcement.enforced : null,
             decoration: InputDecoration(
               filled: true,
               fillColor: const Color(0xFFADD6E0).withOpacity(0.44),
