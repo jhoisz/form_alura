0d73f4f00ab3c04824f9af6e7f2ede908b4615b1
jhoisz <jvrda@ufpi.edu.br>
2022-02-20 00:19:11 -0300

    feat: form validation

--- lib/form/form_page.dart
+++ lib/form/form_page.dart
@@ -26,145 +26,149 @@ class _FormPageState extends State<FormPage> {
   final _cityController = TextEditingController();
   final _stateController = TextEditingController();
 
+  // create function for count one more one
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 60,
-            left: 35.0,
-            right: 35.0,
-            bottom: 15.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Cadastro\nde endereço',
-                style: GoogleFonts.poppins(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 35.0,
+          right: 35.0,
+          bottom: 15.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Cadastro\nde endereço',
+              style: GoogleFonts.poppins(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    buildTextField(
-                      'Nome',
-                      TextInputType.name,
-                      context,
-                      _nameController,
-                    ),
-                    buildTextField(
-                      'Telefone',
-                      TextInputType.number,
-                      context,
-                      _phoneController,
-                    ),
-                    Row(
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: buildTextField(
-                            'CEP',
-                            TextInputType.number,
-                            context,
-                            _cepController,
-                          ),
+                        buildTextField(
+                          'Nome',
+                          TextInputType.name,
+                          context,
+                          _nameController,
                         ),
-                        const SizedBox(width: 15.0),
-                        Expanded(
-                          child: buildTextField(
-                            'Número',
-                            TextInputType.phone,
-                            context,
-                            _numberController,
-                          ),
+                        buildTextField(
+                          'Telefone',
+                          TextInputType.number,
+                          context,
+                          _phoneController,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: buildTextField(
+                                'CEP',
+                                TextInputType.number,
+                                context,
+                                _cepController,
+                              ),
+                            ),
+                            const SizedBox(width: 15.0),
+                            Expanded(
+                              child: buildTextField(
+                                'Número',
+                                TextInputType.phone,
+                                context,
+                                _numberController,
+                              ),
+                            ),
+                          ],
+                        ),
+                        buildTextField(
+                          'Logradouro',
+                          TextInputType.streetAddress,
+                          context,
+                          _streetController,
+                          maxLengthText: 100,
+                          validator: (text) {
+                            if ((text?.length ?? 0) < 10) {
+                              return 'Mínimo de 10 caracteres';
+                            }
+                            return null;
+                          },
+                        ),
+                        buildTextField(
+                          'Bairro',
+                          TextInputType.text,
+                          context,
+                          _neighborhoodController,
+                        ),
+                        buildTextField(
+                          'Complemento',
+                          TextInputType.text,
+                          context,
+                          _complementController,
+                          validator: (value) {
+                            return null;
+                          },
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: buildTextField(
+                                'Estado',
+                                TextInputType.text,
+                                context,
+                                _stateController,
+                              ),
+                            ),
+                            const SizedBox(width: 15.0),
+                            Expanded(
+                              child: buildTextField(
+                                'Cidade',
+                                TextInputType.text,
+                                context,
+                                _cityController,
+                                isLast: true,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    buildTextField(
-                      'Logradouro',
-                      TextInputType.streetAddress,
-                      context,
-                      _streetController,
-                      maxLengthText: 100,
-                    ),
-                    buildTextField(
-                      'Bairro',
-                      TextInputType.text,
-                      context,
-                      _neighborhoodController,
-                    ),
-                    buildTextField(
-                      'Complemento',
-                      TextInputType.text,
-                      context,
-                      _complementController,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: buildTextField(
-                            'Estado',
-                            TextInputType.text,
-                            context,
-                            _stateController,
-                          ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 16,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
-                        const SizedBox(width: 15.0),
-                        Expanded(
-                          child: buildTextField(
-                            'Cidade',
-                            TextInputType.text,
-                            context,
-                            _cityController,
-                          ),
+                        primary: const Color(0xFF0123FF),
+                      ),
+                      onPressed: () {
+                        validateForm();
+                      },
+                      child: Text(
+                        'Salvar',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
+                      ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-              const SizedBox(height: 20.0),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 16,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    primary: const Color(0xFF0123FF),
-                  ),
-                  onPressed: () {
-                    address = Address(
-                      name: _nameController.text,
-                      phone: _phoneController.text,
-                      cep: _cepController.text,
-                      number: _numberController.text,
-                      street: _streetController.text,
-                      neighborhood: _neighborhoodController.text,
-                      complement: _complementController.text,
-                      state: _stateController.text,
-                      city: _cityController.text,
-                    );
-
-                    debugPrint(address?.getAddress().toString());
-                  },
-                  child: Text(
-                    'Salvar',
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -176,6 +180,8 @@ class _FormPageState extends State<FormPage> {
     BuildContext context,
     TextEditingController controller, {
     int? maxLengthText,
+    String? Function(String?)? validator,
+    bool isLast = false,
   }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -184,25 +190,37 @@ class _FormPageState extends State<FormPage> {
           name,
           style: GoogleFonts.poppins(fontSize: 16),
         ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 16,
-          child: TextFormField(
-            controller: controller,
-            // validator: (value) =>
-            //     value!.isEmpty ? 'Email cannot be blank' : null,
-            keyboardType: type,
-            maxLength: maxLengthText,
-            maxLengthEnforcement:
-                maxLengthText != null ? MaxLengthEnforcement.enforced : null,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: const Color(0xFFADD6E0).withOpacity(0.44),
-              counterText: '',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: BorderSide.none,
+        TextFormField(
+          controller: controller,
+          validator: validator ??
+              (value) {
+                return (value?.isEmpty ?? true)
+                    ? 'Campo $name não pode ficar vazio'
+                    : null;
+              },
+          keyboardType: type,
+          textInputAction: isLast ? TextInputAction.done : TextInputAction.next,
+          maxLength: maxLengthText,
+          maxLengthEnforcement:
+              maxLengthText != null ? MaxLengthEnforcement.enforced : null,
+          style: GoogleFonts.poppins(fontSize: 14.0),
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 15.0,
+              horizontal: 10.0,
+            ),
+            errorStyle: GoogleFonts.poppins(
+              fontSize: 11.0,
+              textStyle: const TextStyle(
+                overflow: TextOverflow.visible,
               ),
+            ),
+            filled: true,
+            fillColor: const Color(0xFFADD6E0).withOpacity(0.44),
+            counterText: '',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
             ),
           ),
         ),
@@ -210,12 +228,23 @@ class _FormPageState extends State<FormPage> {
     );
   }
 
-  // void validate() {
-  //   final FormState? form = _formKey.currentState;
-  //   if (form!.validate()) {
-  //     print('Form is valid');
-  //   } else {
-  //     print('Form is not valid');
-  //   }
-  // }
+  void validateForm() {
+    final FormState? form = _formKey.currentState;
+    if (form?.validate() ?? false) {
+      address = Address(
+        name: _nameController.text,
+        phone: _phoneController.text,
+        cep: _cepController.text,
+        number: _numberController.text,
+        street: _streetController.text,
+        neighborhood: _neighborhoodController.text,
+        complement: _complementController.text,
+        state: _stateController.text,
+        city: _cityController.text,
+      );
+      debugPrint(address?.getAddress().toString());
+    } else {
+      debugPrint('Formulário inválido');
+    }
+  }
 }
